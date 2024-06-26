4eb7cecd5f4d9deebb049f7a4742967c4a2cdb0d
winningJP <github@winning.moe>
2024-06-26 12:14:44 +0900

    わけわからん

--- work6.py
+++ work6.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import random
+# import random
 
 # ↓↓↓ お約束のコード ↓↓↓
 window = tk.Tk()
@@ -10,23 +10,52 @@ fg_color = "#FFFFFF"  # 白
 window.configure(bg=bg_color)
 # ↑↑↑ お約束のコード ↑↑↑
 
-list = []
+list = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 
-def game():
-    #
+# def game():
 
 
-    # 入力フィールドの作成
-label1 = tk.Label(window, text=text, bg=bg_color, fg=fg_color)
-label1.pack(pady=10)
-
-type_entry = tk.Entry(window, bg=fg_color, fg=bg_color)
-type_entry.pack(pady=10)
+# 入力フィールドの作成
+label = tk.Label(window, text="マルバツゲーム", bg=bg_color, fg=fg_color)
+label.grid(row=0, column=0)
 
 # ボタンの作成
-button1 = tk.Button(window, text="OK", command=typing)
-button1.pack(pady=10)
+box_0 = tk.Button(window, text=" ")
+box_0.grid(row=2, column=1)
+box_1 = tk.Button(window, text=" ")
+box_1.grid(row=2, column=2)
+box_2 = tk.Button(window, text=" ")
+box_2.grid(row=2, column=3)
+
+box_3 = tk.Button(window, text=" ")
+box_3.grid(row=3, column=1)
+box_4 = tk.Button(window, text=" ")
+box_4.grid(row=3, column=2)
+box_5 = tk.Button(window, text=" ")
+box_5.grid(row=3, column=3)
+
+box_6 = tk.Button(window, text=" ")
+box_6.grid(row=4, column=1)
+box_7 = tk.Button(window, text=" ")
+box_7.grid(row=4, column=2)
+box_8 = tk.Button(window, text=" ")
+box_8.grid(row=4, column=3)
+
+# box_3 = tk.Button(window, text=" ")
+# box_3.pack(side=tk.LEFT, anchor=tk.N)
+# box_4 = tk.Button(window, text=" ")
+# box_4.pack(side=tk.LEFT, anchor=tk.N)
+# box_5 = tk.Button(window, text=" ")
+# box_5.pack(side=tk.LEFT, anchor=tk.N)
+
+# box_6 = tk.Button(window, text=" ")
+# box_6.pack(side=tk.LEFT, anchor=tk.N)
+# box_7 = tk.Button(window, text=" ")
+# box_7.pack(side=tk.LEFT, anchor=tk.N)
+# box_8 = tk.Button(window, text=" ")
+# box_8.pack(side=tk.LEFT, anchor=tk.N)
+
 
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
