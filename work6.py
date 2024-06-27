43cf7d8ea44d8d357f88f562787046762932fa55
winningJP <github@winning.moe>
2024-06-27 09:46:38 +0900

    こみっと

--- work6.py
+++ work6.py
@@ -13,15 +13,14 @@ window.configure(bg=bg_color)
 list = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 
-# def game():
+def game(box):
 
-
-# 入力フィールドの作成
+    # 入力フィールドの作成
 label = tk.Label(window, text="マルバツゲーム", bg=bg_color, fg=fg_color)
 label.grid(row=0, column=0)
 
 # ボタンの作成
-box_0 = tk.Button(window, text=" ")
+box_0 = tk.Button(window, text=" ", command=game(0))
 box_0.grid(row=2, column=1)
 box_1 = tk.Button(window, text=" ")
 box_1.grid(row=2, column=2)
@@ -41,20 +40,6 @@ box_7 = tk.Button(window, text=" ")
 box_7.grid(row=4, column=2)
 box_8 = tk.Button(window, text=" ")
 box_8.grid(row=4, column=3)
-
-# box_3 = tk.Button(window, text=" ")
-# box_3.pack(side=tk.LEFT, anchor=tk.N)
-# box_4 = tk.Button(window, text=" ")
-# box_4.pack(side=tk.LEFT, anchor=tk.N)
-# box_5 = tk.Button(window, text=" ")
-# box_5.pack(side=tk.LEFT, anchor=tk.N)
-
-# box_6 = tk.Button(window, text=" ")
-# box_6.pack(side=tk.LEFT, anchor=tk.N)
-# box_7 = tk.Button(window, text=" ")
-# box_7.pack(side=tk.LEFT, anchor=tk.N)
-# box_8 = tk.Button(window, text=" ")
-# box_8.pack(side=tk.LEFT, anchor=tk.N)
 
 
 # ↓↓↓ お約束のコード ↓↓↓
