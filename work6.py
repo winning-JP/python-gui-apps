0df9def34a55f9b1c64f7ae0da2cf63a1809378a
hirokihiro <hirokihiro117117@gmail.com>
2024-06-27 09:59:06 +0900

    kaki

--- work6.py
+++ work6.py
@@ -15,30 +15,30 @@ list = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 def game(box):
 
-    # 入力フィールドの作成
+# 入力フィールドの作成
 label = tk.Label(window, text="マルバツゲーム", bg=bg_color, fg=fg_color)
 label.grid(row=0, column=0)
 
 # ボタンの作成
-box_0 = tk.Button(window, text=" ", command=game(0))
+box_0 = tk.Button(window, text=" ", command=game("box_0"))
 box_0.grid(row=2, column=1)
-box_1 = tk.Button(window, text=" ")
+box_1 = tk.Button(window, text=" ", command=game("box_1"))
 box_1.grid(row=2, column=2)
-box_2 = tk.Button(window, text=" ")
+box_2 = tk.Button(window, text=" ", command=game("box_2"))
 box_2.grid(row=2, column=3)
 
-box_3 = tk.Button(window, text=" ")
+box_3 = tk.Button(window, text=" ", command=game("box_3"))
 box_3.grid(row=3, column=1)
-box_4 = tk.Button(window, text=" ")
+box_4 = tk.Button(window, text=" ", command=game("box_4"))
 box_4.grid(row=3, column=2)
-box_5 = tk.Button(window, text=" ")
+box_5 = tk.Button(window, text=" ", command=game("box_5"))
 box_5.grid(row=3, column=3)
 
-box_6 = tk.Button(window, text=" ")
+box_6 = tk.Button(window, text=" ", command=game("box_6"))
 box_6.grid(row=4, column=1)
-box_7 = tk.Button(window, text=" ")
+box_7 = tk.Button(window, text=" ", command=game("box_7"))
 box_7.grid(row=4, column=2)
-box_8 = tk.Button(window, text=" ")
+box_8 = tk.Button(window, text=" ", command=game("box_8"))
 box_8.grid(row=4, column=3)
 
 
